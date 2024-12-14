7f140585b0556d4fe860e57c0efa70f651a4c01b
sheng.yao <1076009113@qq.com>
2024-12-14 19:34:12 +0800

    add model loader module

---
+++ diffusion/model/model_loader.py
@@ -0,0 +1,28 @@
+from diffusion.clip import CLIP
+from diffusion.vae.encoder import VAE_Encoder
+from diffusion.vae.decoder import VAE_Decoder
+from diffusion.model import Diffusion
+import model_converter
+
+
+def preload_models_from_standard_weights(ckpt_path, device):
+    state_dict = model_converter.load_from_standard_weights(ckpt_path, device)
+
+    encoder = VAE_Encoder().to(device)
+    encoder.load_state_dict(state_dict['encoder'], strict=True)
+
+    decoder = VAE_Decoder().to(device)
+    decoder.load_state_dict(state_dict['decoder'], strict=True)
+
+    diffusion = Diffusion().to(device)
+    diffusion.load_state_dict(state_dict['diffusion'], strict=True)
+
+    clip = CLIP().to(device)
+    clip.load_state_dict(state_dict['clip'], strict=True)
+
+    return {
+        'clip': clip,
+        'encoder': encoder,
+        'decoder': decoder,
+        'diffusion': diffusion,
+    }
