441564c73b28b5a0061e7e50b73b54e9da2e2de9
sheng.yao <1076009113@qq.com>
2024-12-14 21:53:46 +0800

    add run demo

--- diffusion/model/pipeline.py
+++ diffusion/model/pipeline.py
@@ -45,32 +45,32 @@ def generate(
         clip.to(device)
 
         if do_cfg:
-            # Convert into a list of length Seq_Len=77
+            # Convert into a list of length Seq_len=77
             cond_tokens = tokenizer.batch_encode_plus(
                 [prompt], padding="max_length", max_length=77
             ).input_ids
-            # (Batch_Size, Seq_Len)
+            # (Batch_size, Seq_len)
             cond_tokens = torch.tensor(cond_tokens, dtype=torch.long, device=device)
-            # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
+            # (Batch_size, Seq_len) -> (Batch_size, Seq_len, dim)
             cond_context = clip(cond_tokens)
-            # Convert into a list of length Seq_Len=77
+            # Convert into a list of length Seq_len=77
             uncond_tokens = tokenizer.batch_encode_plus(
                 [uncond_prompt], padding="max_length", max_length=77
             ).input_ids
-            # (Batch_Size, Seq_Len)
+            # (Batch_size, Seq_len)
             uncond_tokens = torch.tensor(uncond_tokens, dtype=torch.long, device=device)
-            # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
+            # (Batch_size, Seq_len) -> (Batch_size, Seq_len, dim)
             uncond_context = clip(uncond_tokens)
-            # (Batch_Size, Seq_Len, Dim) + (Batch_Size, Seq_Len, Dim) -> (2 * Batch_Size, Seq_Len, Dim)
+            # (Batch_size, Seq_len, dim) + (Batch_size, Seq_len, dim) -> (2 * Batch_size, Seq_len, dim)
             context = torch.cat([cond_context, uncond_context])
         else:
-            # Convert into a list of length Seq_Len=77
+            # Convert into a list of length Seq_len=77
             tokens = tokenizer.batch_encode_plus(
                 [prompt], padding="max_length", max_length=77
             ).input_ids
-            # (Batch_Size, Seq_Len)
+            # (Batch_size, Seq_len)
             tokens = torch.tensor(tokens, dtype=torch.long, device=device)
-            # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
+            # (Batch_size, Seq_len) -> (Batch_size, Seq_len, dim)
             context = clip(tokens)
         to_idle(clip)
 
@@ -85,30 +85,30 @@ def generate(
             encoder = models["encoder"]
             encoder.to(device)
             input_image_tensor = input_image.resize((WIDTH, HEIGHT))
-            # (Height, Width, Channel)
+            # (height, width, channel)
             input_image_tensor = np.array(input_image_tensor)
-            # (Height, Width, Channel) -> (Height, Width, Channel)
+            # (height, width, channel) -> (height, width, channel)
             input_image_tensor = torch.tensor(input_image_tensor, dtype=torch.float32, device=device)
-            # (Height, Width, Channel) -> (Height, Width, Channel)
+            # (height, width, channel) -> (height, width, channel)
             input_image_tensor = rescale(input_image_tensor, (0, 255), (-1, 1))
-            # (Height, Width, Channel) -> (Batch_Size, Height, Width, Channel)
+            # (height, width, channel) -> (Batch_size, height, width, channel)
             input_image_tensor = input_image_tensor.unsqueeze(0)
-            # (Batch_Size, Height, Width, Channel) -> (Batch_Size, Channel, Height, Width)
+            # (Batch_size, height, width, channel) -> (Batch_size, channel, height, width)
             input_image_tensor = input_image_tensor.permute(0, 3, 1, 2)
 
-            # (Batch_Size, 4, Latents_Height, Latents_Width)
+            # (Batch_size, 4, Latents_height, Latents_width)
             encoder_noise = torch.randn(latents_shape, generator=generator, device=device)
-            # (Batch_Size, 4, Latents_Height, Latents_Width)
+            # (Batch_size, 4, Latents_height, Latents_width)
             latents = encoder(input_image_tensor, encoder_noise)
 
             # Add noise to the latents (the encoded input image)
-            # (Batch_Size, 4, Latents_Height, Latents_Width)
+            # (Batch_size, 4, Latents_height, Latents_width)
             sampler.set_strength(strength=strength)
             latents = sampler.add_noise(latents, sampler.timesteps[0])
 
             to_idle(encoder)
         else:
-            # (Batch_Size, 4, Latents_Height, Latents_Width)
+            # (Batch_size, 4, Latents_height, Latents_width)
             latents = torch.randn(latents_shape, generator=generator, device=device)
 
         diffusion = models["diffusion"]
@@ -118,35 +118,34 @@ def generate(
         for i, timestep in enumerate(timesteps):
             # (1, 320)
             time_embedding = get_time_embedding(timestep).to(device)
-
-            # (Batch_Size, 4, Latents_Height, Latents_Width)
+            # (Batch_size, 4, Latents_height, Latents_width)
             model_input = latents
 
             if do_cfg:
-                # (Batch_Size, 4, Latents_Height, Latents_Width) -> (2 * Batch_Size, 4, Latents_Height, Latents_Width)
+                # (Batch_size, 4, Latents_height, Latents_width) -> (2 * Batch_size, 4, Latents_height, Latents_width)
                 model_input = model_input.repeat(2, 1, 1, 1)
 
             # model_output is the predicted noise
-            # (Batch_Size, 4, Latents_Height, Latents_Width) -> (Batch_Size, 4, Latents_Height, Latents_Width)
+            # (Batch_size, 4, Latents_height, Latents_width) -> (Batch_size, 4, Latents_height, Latents_width)
             model_output = diffusion(model_input, context, time_embedding)
 
             if do_cfg:
                 output_cond, output_uncond = model_output.chunk(2)
                 model_output = cfg_scale * (output_cond - output_uncond) + output_uncond
 
-            # (Batch_Size, 4, Latents_Height, Latents_Width) -> (Batch_Size, 4, Latents_Height, Latents_Width)
+            # (Batch_size, 4, Latents_height, Latents_width) -> (Batch_size, 4, Latents_height, Latents_width)
             latents = sampler.step(timestep, latents, model_output)
 
         to_idle(diffusion)
 
         decoder = models["decoder"]
         decoder.to(device)
-        # (Batch_Size, 4, Latents_Height, Latents_Width) -> (Batch_Size, 3, Height, Width)
+        # (Batch_size, 4, Latents_height, Latents_width) -> (Batch_size, 3, height, width)
         images = decoder(latents)
         to_idle(decoder)
 
         images = rescale(images, (-1, 1), (0, 255), clamp=True)
-        # (Batch_Size, Channel, Height, Width) -> (Batch_Size, Height, Width, Channel)
+        # (Batch_size, channel, height, width) -> (Batch_size, height, width, channel)
         images = images.permute(0, 2, 3, 1)
         images = images.to("cpu", torch.uint8).numpy()
         return images[0]
@@ -164,9 +163,9 @@ def rescale(x, old_range, new_range, clamp=False):
 
 
 def get_time_embedding(timestep):
-    # Shape: (160,)
+    # shape: (160,)
     freqs = torch.pow(10000, -torch.arange(start=0, end=160, dtype=torch.float32) / 160)
-    # Shape: (1, 160)
+    # shape: (1, 160)
     x = torch.tensor([timestep], dtype=torch.float32)[:, None] * freqs[None]
-    # Shape: (1, 160 * 2)
+    # shape: (1, 160 * 2)
     return torch.cat([torch.cos(x), torch.sin(x)], dim=-1)
